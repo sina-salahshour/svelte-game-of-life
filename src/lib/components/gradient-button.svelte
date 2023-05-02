03a8adb9df0d82e26e0d175d4c1097fd5861a874
Seth <sina_s@felixin.io>
2023-05-02 21:20:56 +0330

    fix: small twaks

--- src/lib/components/gradient-button.svelte
+++ src/lib/components/gradient-button.svelte
@@ -8,13 +8,13 @@
 
 <button on:click {type} class={className}>
 	<HoverLightBorder
-		class={'transition rounded-md active:scale-95 hover:scale-105'}
+		class={'transition rounded-md active:scale-[0.99] hover:scale-[1.01]'}
 		borderClass="rounded-md"
 		lightColor="#fff"
 		wrapperClass="bg-black"
 	>
 		<div
-			class="w-full px-4 overflow-hidden transition bg-white bg-opacity-20 active:bg-opacity-40 hover:bg-opacity-10"
+			class="w-full px-4 overflow-hidden transition bg-white bg-opacity-20 active:bg-opacity-30 hover:bg-opacity-10"
 		>
 			<slot />
 		</div>
