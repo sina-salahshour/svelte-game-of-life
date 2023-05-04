f314828b9c5eae67b25cd8548f12dedd04cbb8c2
Seth <sina_s@felixin.io>
2023-05-04 15:13:34 +0330

    fix: modal types

--- src/lib/stores/modal.store.ts
+++ src/lib/stores/modal.store.ts
@@ -1,14 +1,15 @@
 import { createStore } from '$lib/utils/create-store.util';
+import type { ComponentType, SvelteComponentTyped } from 'svelte';
 
 export const {
 	store: modalStore,
 	dispatchReducers: { hideModal, showModal }
 } = createStore({
-	initialState: { isVisible: false, component: null as ConstructorOfATypedSvelteComponent | null },
+	initialState: { isVisible: false, component: null as ComponentType<SvelteComponentTyped> | null },
 	reducers: {
-		showModal(state, componnet: ConstructorOfATypedSvelteComponent) {
+		showModal(state, component: ComponentType<SvelteComponentTyped>) {
 			state.isVisible = true;
-			state.component = componnet as (typeof state)['component'];
+			state.component = component as (typeof state)['component'];
 		},
 		hideModal(state) {
 			state.isVisible = false;
