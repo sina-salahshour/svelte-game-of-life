e5c31013e0ac7b07642f4cd0b5ed95d6098a3cd9
Seth <sina_s@felixin.io>
2023-05-05 11:40:55 +0330

    feat: lucia prisma sentry config

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,19 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+		interface Locals {
+			auth: import('lucia-auth').AuthRequest;
+		}
+	}
+}
+
+/// <reference types="lucia-auth" />
+declare global {
+	namespace Lucia {
+		type Auth = import('$lib/server/lucia').Auth;
+		type UserAttributes = {
+			username: string;
+		};
 	}
 }
 
---
+++ src/hooks.server.ts
@@ -0,0 +1,28 @@
+import { auth } from '$lib/server/lucia';
+import type { Handle, HandleServerError } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.auth = auth.handleRequest(event);
+	return await resolve(event);
+};
+
+import * as Sentry from '@sentry/svelte';
+import { Prisma } from '@prisma/client';
+
+Sentry.init({
+	dsn: process.env.SENTRY_DSN,
+	integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+export const handleError = (({ error, event }) => {
+	Sentry.captureException(error, { extra: { event } });
+	if (error instanceof Prisma.PrismaClientKnownRequestError) {
+		// The .code property can be accessed in a type-safe manner
+	}
+	return { message: 'Internal Error' };
+}) satisfies HandleServerError;
