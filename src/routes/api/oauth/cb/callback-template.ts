b311a9144694a03fd91e7954e20fe55229825b55
Seth <sina_s@felixin.io>
2023-05-07 16:18:14 +0330

    wip: check callback error

--- src/routes/api/oauth/cb/callback-template.ts
+++ src/routes/api/oauth/cb/callback-template.ts
@@ -6,31 +6,35 @@ import type { Auth } from 'lucia-auth';
 
 export function authCallbackTemplate<T extends OAuthProvider<any>>(provider: T) {
 	const GET: RequestHandler = async ({ cookies, url, locals }) => {
-		// get code and state params from url
-		const { code, state } = oauthCallBackParams.parse({
-			code: url.searchParams.get('code'),
-			state: url.searchParams.get('state')
-		});
-		// get stored state from cookies
-		const storedState = cookies.get('oauth_state');
-
-		// validate state
-		if (state !== storedState) throw new Response(null, { status: 401 });
-
-		const { existingUser, providerUser, createUser } = await provider.validateCallback(code);
-
-		const getUser = async () => {
-			if (existingUser) return existingUser;
-			// create a new user if the user does not exist
-			return await createUser({
-				// attributes
-				username: providerUser.login || providerUser.email
+		try {
+			// get code and state params from url
+			const { code, state } = oauthCallBackParams.parse({
+				code: url.searchParams.get('code'),
+				state: url.searchParams.get('state')
 			});
-		};
-		const user = await getUser();
-		const session = await auth.createSession(user.userId);
-		locals.auth.setSession(session);
-		throw redirect(302, '/');
+			// get stored state from cookies
+			const storedState = cookies.get('oauth_state');
+
+			// validate state
+			if (state !== storedState) throw new Response(null, { status: 401 });
+
+			const { existingUser, providerUser, createUser } = await provider.validateCallback(code);
+
+			const getUser = async () => {
+				if (existingUser) return existingUser;
+				// create a new user if the user does not exist
+				return await createUser({
+					// attributes
+					username: providerUser.login || providerUser.email
+				});
+			};
+			const user = await getUser();
+			const session = await auth.createSession(user.userId);
+			locals.auth.setSession(session);
+			throw redirect(302, '/');
+		} catch (err) {
+			return new Response(JSON.stringify(err));
+		}
 	};
 	return GET;
 }
