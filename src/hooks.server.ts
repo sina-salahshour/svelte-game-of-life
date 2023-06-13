11268e13441837ef15c5e1b8e97bda529a00739e
Sina <sina.salahshour.32@gmail.com>
2023-06-14 02:19:04 +0330

    fix: handle private routes in hook level

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,7 @@
 import { env } from '$env/dynamic/public';
-import type { Handle } from '@sveltejs/kit';
+import routes, { isRoutePrivate } from '$lib/configs/routes';
+import { redirect, type Handle } from '@sveltejs/kit';
+import { StatusCodes } from 'http-status-codes';
 import PocketBase from 'pocketbase';
 
 export const handle = (async ({ event, resolve }) => {
@@ -7,8 +9,10 @@ export const handle = (async ({ event, resolve }) => {
 	pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 	event.locals.pb = pb;
 
+	if (!pb.authStore.isValid && isRoutePrivate(event.url.pathname)) {
+		throw redirect(StatusCodes.TEMPORARY_REDIRECT, routes.user.login);
+	}
 	const response = await resolve(event);
-
 	response.headers.set('set-cookie', pb.authStore.exportToCookie({ secure: false }));
 	return response;
 }) satisfies Handle;
