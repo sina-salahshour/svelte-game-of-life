efa918f58c6e5b8242c30298463b00a16ac14672
Sina <sina.salahshour.32@gmail.com>
2023-06-14 02:22:24 +0330

    fix: handle private routes in hook level

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,11 +8,11 @@ export const handle = (async ({ event, resolve }) => {
 	const pb = new PocketBase(env.PUBLIC_POCKETBASE_URL);
 	pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 	event.locals.pb = pb;
+	const response = await resolve(event);
 
 	if (!pb.authStore.isValid && isRoutePrivate(event.url.pathname)) {
 		throw redirect(StatusCodes.TEMPORARY_REDIRECT, routes.user.login);
 	}
-	const response = await resolve(event);
 	response.headers.set('set-cookie', pb.authStore.exportToCookie({ secure: false }));
 	return response;
 }) satisfies Handle;
