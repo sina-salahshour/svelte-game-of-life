a89410a97b13caad49b393ba25ac403f3d2bf880
Sina <sina.salahshour.32@gmail.com>
2023-06-14 01:06:25 +0330

    fix: logout redirect

--- src/routes/user/(private)/logout/+page.svelte
+++ src/routes/user/(private)/logout/+page.svelte
@@ -2,8 +2,11 @@
 	import { invalidate } from '$app/navigation';
 	import { DependKeys } from '$lib/configs/depend-keys';
 	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+	export let data: PageData;
 
 	onMount(async () => {
+		data.user;
 		await fetch('', { method: 'POST', body: new FormData() });
 		await invalidate(DependKeys.AUTH);
 	});
