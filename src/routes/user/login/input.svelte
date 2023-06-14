69e391c1ff299e482cdf715c1c24125da4faf479
Sina <sina.salahshour.32@gmail.com>
2023-06-14 19:16:35 +0330

    feat: input error and focus style

--- src/routes/user/login/input.svelte
+++ src/routes/user/login/input.svelte
@@ -9,22 +9,47 @@
 	export let placeholder = '';
 	export { className as class };
 	export let required = false;
+	export let error = '';
 </script>
 
-<input
-	class={classNames(className, 's-input')}
-	{type}
-	{value}
-	{name}
-	{id}
-	{placeholder}
-	{required}
-/>
+<div class={classNames(className, 's-wrapper')}>
+	<input
+		class={'s-input'}
+		{type}
+		{value}
+		{name}
+		{id}
+		{placeholder}
+		{required}
+		data-error={String(!!error)}
+	/>
+	{#if error}
+		<div class="s-error">
+			<img class="s-error-icon" src="/icons/error.png" alt="" />
+			{error}
+		</div>
+	{/if}
+</div>
 
 <style lang="postcss">
+	.s-wrapper {
+		@apply w-full pb-[27px] relative;
+	}
 	.s-input {
-		@apply p-5 bg-[#1B1B1B] rounded-xl border-[#3B3A3A3A] border-[1px] w-full outline-none;
+		@apply p-5 bg-[#1B1B1B] rounded-xl border-[#3B3A3A3A] border-[1px] w-full outline-none transition;
 		@apply font-semibold text-xl text-[#fffc];
 		@apply placeholder:text-[#3B3A3A];
+		&:focus {
+			box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.6);
+		}
+		&[data-error='true'] {
+			box-shadow: 0px 0px 15px rgba(253, 45, 45, 0.6);
+		}
+	}
+	.s-error {
+		@apply text-[#FD2D2D] font-medium text-sm leading-[17px] mt-[10px] absolute bottom-0 flex gap-[6px];
+	}
+	.s-error-icon {
+		@apply w-4 h-4;
 	}
 </style>
